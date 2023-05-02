e2a4b0e3f9ca7339761942d8b0df88b3f84227b0
douze <douze@users.noreply.github.com>
2023-05-02 23:27:55 +0200

    Always display left menu

---
+++ assets/javascripts/main.js
@@ -0,0 +1,18 @@
+const menuItems = document.querySelectorAll('#sidebar li');
+
+// Get vendor transition property
+const docElemStyle = document.documentElement.style;
+const transitionProp = typeof docElemStyle.transition == 'string' ?
+  'transition' : 'WebkitTransition';
+
+// Animate sidebar menu items
+function animateMenuItems() {
+  for (let i = 0; i < menuItems.length; i++) {
+    const item = menuItems[i];
+    // Stagger transition with transitionDelay
+    item.style[transitionProp + 'Delay'] = (i * 75) + 'ms';
+    item.classList.toggle('is--moved');
+  }
+};
+
+animateMenuItems();
